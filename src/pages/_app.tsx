31b33a17b10d49eb947606941008193ca9e1e277
Arthur Cortez <t.multimidia4@al.senai.br>
2024-03-04 08:18:13 -0300

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GlobalContextProvider } from "../context";
 import { useState } from "react";
+import { UsersInterface } from "../types/UsersInterface";
 
 const theme = extendTheme({});
 
