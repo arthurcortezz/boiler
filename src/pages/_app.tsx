40a4d18cc056fd1c4f48438a197d2ab3090a6f58
Arthur Cortez <t.multimidia4@al.senai.br>
2024-03-04 08:21:40 -0300

    deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react";
+
 import { GlobalContextProvider } from "../context";
-import { useState } from "react";
 import { UsersInterface } from "../types/UsersInterface";
 
 const theme = extendTheme({});
